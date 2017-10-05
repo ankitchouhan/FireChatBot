7efabe6855fb22298aa68f7c16aa13f907c2ad97
Ankit Chouhan <chauhan.ankit.19@gmail.com>
2017-10-05 22:29:56 +0530

    contact sync

---
+++ app/src/main/java/com/firechatbot/activities/ChatActivity.java
@@ -0,0 +1,8 @@
+package com.firechatbot.activities;
+
+/**
+ * Created by admin1 on 5/10/17.
+ */
+
+public class ChatActivity {
+}
---
+++ app/src/main/java/com/firechatbot/interfaces/OnContactsReceived.java
@@ -0,0 +1,8 @@
+package com.firechatbot.interfaces;
+
+/**
+ * Created by admin1 on 5/10/17.
+ */
+
+public interface OnContactsReceived {
+}
